46601b49e3ca5e370e87f29405de1f797cf5c01f
UsmanShahid-Developer <usmanshahidite@gmail.com>
2023-02-20 22:04:48 +0500

    change style

---
+++ practice.js
@@ -0,0 +1,5 @@
+function changeStyle(){
+    document.getElementById('change2').style.color="rgb(212, 212, 96)"
+    document.getElementById('change2').style.fontSize="70px"
+}
+alert("I Am Usman Shahid.")
